8b5dd508378f7f9643f42f1ccc667a0248ff3486
RockSolidRobotics4656 <RockSolid4656@isd381.org>
2026-02-02 19:13:13 -0600

    github test complete

--- src/main/java/frc/robot/Robot.java
+++ src/main/java/frc/robot/Robot.java
@@ -1,7 +1,3 @@
-//GITHUB TEST
-
-// hello github!
-// again on 2
 // Copyright (c) FIRST and other WPILib contributors.
 // Open Source Software; you can modify and/or share it under the terms of
 // the WPILib BSD license file in the root directory of this project.
