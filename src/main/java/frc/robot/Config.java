88a97658e604a7dd070cfb1f076b230aaaa80da7
RockSolidRobotics4656 <rocksolid4656@isd381.org>
2026-02-02 19:26:30 -0600

    New Config File

---
+++ src/main/java/frc/robot/Config.java
@@ -0,0 +1,11 @@
+package frc.robot;
+
+import com.revrobotics.spark.SparkLowLevel.MotorType;
+
+public class Config {
+    public static final int kDriveLeftLeadCANID = 2;
+    public static final int kDriveLeftFollowCANID = 3;
+    public static final int kDriveRightLeadCANID = 1;
+    public static final int kCriveRightFollowCANID = 4;
+    public static final MotorType kDriveMotorType = MotorType.kBrushless;
+}
