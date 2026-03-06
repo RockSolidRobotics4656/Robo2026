b92d361e50137f5ed1e09860884cb09e3df387f1
RockSolidRobotics4656 <RockSolid4656@isd381.org>
2026-03-05 21:58:23 -0600

    alright, now it's merged?

--- src/main/java/frc/robot/Robot.java
+++ src/main/java/frc/robot/Robot.java
@@ -2,7 +2,7 @@
 // Open Source Software; you can modify and/or share it under the terms of
 // the WPILib BSD license file in the root directory of this project.
 
-package frc;
+package frc.robot;
 // this links to the Config file, where constants are stored.
 import frc.robot.Config.*;
 
